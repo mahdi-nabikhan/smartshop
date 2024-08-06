80a6859b38c66585ed1abb4175b294e978446165
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2024-08-06 20:29:27 +0330

    templates and static file added

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website.apps.WebsiteConfig',
+    'orders.apps.OrdersConfig',
+    'accounts.apps.AccountsConfig',
+    'vendors.apps.VendorsConfig',
+    'customers.apps.CustomersConfig',
+    'dashboards.apps.DashboardsConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'statics']
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
