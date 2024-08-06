4dd6b1a3a00e2f3f49696c4902a2e72043e0a3d5
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2024-08-06 22:58:31 +0330

    model for product and image product in website app added

--- src/customers/models.py
+++ src/customers/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from ..accounts.models import User
-from ..website import Product
-
-
+from accounts.models import User
+from website.models import Product
 # Create your models here.
 
 
--- src/website/models.py
+++ src/website/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    quantity_in_stock = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+    discount = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.name} {self.description}"
+
+
+class ProductImages(models.Model):
+    product_image = models.ImageField(upload_to='product_images')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='image_for_product')
+    title = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title} {self.product.name}'
