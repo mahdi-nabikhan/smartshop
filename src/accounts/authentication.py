61ec7d0cbbc093b690c6e88e8a83b1872ce4c03a
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2024-08-11 17:22:36 +0330

    basebackend for login with phone number added

--- src/accounts/authentication.py
+++ src/accounts/authentication.py
@@ -11,7 +11,7 @@ class PhoneAuthenticationBackend(BaseBackend):
 
             return None
         except User.DoesNotExist:
-            raise ValueError('+=')
+            raise ValueError('this user DoseNot Exist ....')
 
     def get_user(self, user_id):
         try:
